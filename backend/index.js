11b0a27b887f8832de0e180a85263cccde8cc747
Brianna Lamarre <briannalamarre@Briannas-Laptop.local>
2025-07-29 22:11:08 -0400

    trying to fix cors error

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express()
 const port = process.env.PORT || 8080;
 app.use(express.json())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173','https://virtual-makeup-lookbook-and.onrender.com'],
     methods:['GET','POST','PUT','DELETE','OPTIONS'],
     allowedHeaders: ['Content-Type','Authorization','Origin']
 }))
